5e512bc9c4e7b669e8c32b67b72ee51669726c88
jkueb <jerkuebler@gmail.com>
2020-02-22 13:51:48 -0800

    csv function and splitting functions

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use itertools::Itertools;
 use std::time::SystemTime;
 use std::env;
 
+extern crate csv;
+
 fn check_square(input: &Vec<&u32>, size: usize) -> bool {
 
     let diag1: u32 = input[0..size.pow(2)].iter()
@@ -33,43 +35,67 @@ fn check_square(input: &Vec<&u32>, size: usize) -> bool {
     return true;
 }
 
-fn check_permutations(input: &Vec<u32>, size: usize) -> Vec<Vec<&u32>> {
-    let mut partial = Vec::new();
+fn check_permutations(input: &Vec<u32>, size: usize) -> Vec<Vec<u32>> {
+    let mut partial:Vec<Vec<u32>> = Vec::new();
     for square in input.iter().permutations(9) {
         if check_square(&square, size) {
-            partial.push(square);
+            partial.push(square.iter().cloned().cloned().collect());
         }
     }
     return partial;
 }
 
+fn log_to_csv(input: Vec<Vec<u32>>, loc: String) {
+
+    let mut wtr = csv::Writer::from_path(loc)
+        .expect("Couldn't start writer");
+    for square in input {
+        let record = square
+            .iter()
+            .map(|value| value.to_string())
+            .collect::<Vec<String>>();
+        wtr.write_record(record).expect("Couldn't write record");
+    }
+    wtr.flush().expect("Couldn't flush");
+}
+
+fn square_finder(max_val: u32, side: usize) -> Vec<Vec<u32>> {
+    let mut squares: Vec<Vec<u32>> = Vec::new();
+
+    let test = (1..max_val + 1)
+        .map(|j| j)
+        .combinations(9)
+        .collect_vec();
+    test
+        .par_iter()
+        .map(|sq| check_permutations(sq, side))
+        .collect::<Vec<Vec<_>>>()
+        .iter()
+        .for_each(|a| squares.extend_from_slice(&a[..]));
+
+    return squares;
+}
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
     let s_size: usize = args[1].parse::<usize>().expect("First arg not a valid side size");
     let i: u32 = args[2].parse::<u32>().expect("Second arg not a valid max value");
     println!("Side Size: {} \nMax Value: {}", s_size, i);
 
     let now = SystemTime::now();
-    let mut result: Vec<Vec<&u32>> = Vec::new();
 
-    let test = (1..i + 1)
-        .map(|j| j*j)
-        .combinations(9)
-        .collect_vec();
-    test
-        .par_iter()
-        .map(|sq| check_permutations(sq, s_size))
-        .collect::<Vec<Vec<_>>>()
-        .iter()
-        .for_each(|a| result.extend_from_slice(a));
+    let result: Vec<Vec<u32>> = square_finder(i, s_size);
 
     match now.elapsed() {
         Ok(elapsed) => {
-            println!("Run Time: {}", elapsed.as_secs() as f64 + elapsed.subsec_nanos() as f64 * 1e-9);
+            let run = elapsed.as_secs() as f64 + elapsed.subsec_nanos() as f64 * 1e-9;
+            println!("Run Time: {}", run);
             println!("Magic Squares Found: {}", result.len());
-            println!("Magic Squares: {:?}", result);
+            if result.len() > 1 {
+                let str: String = format!("ssize{}maxv{}run{:.1}.csv", s_size, i, run);
+                log_to_csv(result, str)
+            }
         }
         Err(e) => {
             println!("Error: {:?}", e);
