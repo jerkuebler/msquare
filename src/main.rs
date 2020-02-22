e7bebd0bf76830b180bf41ab6129523e6e07e8b0
jerome <jerkuebler@gmail.com>
2020-02-22 14:00:28 -0800

    iterative testing of larger i values

--- src/main.rs
+++ src/main.rs
@@ -66,9 +66,4 @@ fn main() {
             }
         }
     }
-
-    /*let mut input = String::new();
-    std::io::stdin().read_line(&mut input)
-        .ok()
-        .expect("Couldn't read line");*/
 }
